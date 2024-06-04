409b21f9f87a62c1d6068e93684f6eaa7b1ee7e4
tristan-nouzille <tristan.nouzille-77@hotmail.fr>
2024-06-04 18:48:05 +0200

    fin

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+"""""""""""
+Ceci est le code source pour le logiciel du tournoi d'Echecs local
+
+"""""""""""
 import flake8
+import json
+import os
+from datetime import datetime
+
+"""class Matchs:"""
+    
